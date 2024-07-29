f71fc015b9b2ef868e239f59a69129eb5b334cee
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-07-29 13:43:16 -0700

    feat: make homework done

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-$(function() {
-  // YOUR CODE HERE
-})
+$(function () {
+  $("#toggleButton").on("click", function () {
+    if ($("#myParagraph").text().length === 0) {
+      $("#myParagraph").text("This is a paragraph.");
+      return;
+    }
+    $("#myParagraph").text("");
+  });
+
+  $("#colorButton").on("click", function () {
+    $("#colorDiv").css("background-color", "yellow");
+  });
+
+  $("#addClassButton").on("click", function () {
+    $("#classDiv").addClass("className");
+  });
+  $("#removeClassButton").on("click", function () {
+    $("#classDiv").removeAttr("class");
+  });
+
+  $("#fadeInButton").click(function () {
+    $("#fadeDiv").fadeIn();
+  });
+  $("#fadeOutButton").click(function () {
+    $("#fadeDiv").fadeOut();
+  });
+
+  $("#slideUpButton").click(function () {
+    $("#slideDiv").slideUp();
+  });
+  $("#slideDownButton").click(function () {
+    $("#slideDiv").slideDown();
+  });
+});
